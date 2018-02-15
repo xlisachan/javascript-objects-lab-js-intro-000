9be6f08af8b44a62b3dddf81fb477809fc2b69fd
Lisa Chan <xlisachan@gmail.com>
2018-02-15 20:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function deleteFromObjectByKey(object, key){
   delete object.key;
   return object;
 }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}
