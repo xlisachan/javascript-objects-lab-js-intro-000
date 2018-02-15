5c450e36e2974b48f183c3dd4242bff65433e45d
Lisa Chan <xlisachan@gmail.com>
2018-02-15 20:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, {[key]: value});
-  return recipes;
+  object[key]: value;
+  return object;
 }
